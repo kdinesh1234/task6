67d228dd450276b0215529c0f4b35c9aa7183bf0
kdinesh1234 <158142344+kdinesh1234@users.noreply.github.com>
2024-02-11 01:04:51 -0800

    Add files via upload

---
+++ class circle {.js
@@ -0,0 +1,37 @@
+class circle {
+   constructor ( radius, color){
+       this.radius = radius;
+       this.color = color;{
+
+       circle( radius = 1.0 , color = "red")};
+
+     getradius();{
+          returnthis.radius =1.0;
+     }
+    
+      setradius();{
+            this.radius =1.0;
+      }
+
+        getcolor();{
+          return this.color ="red";
+        }
+       
+       
+        setcolor();{
+            this.color ="red";
+        }
+
+       toString();{
+         return  "circle[radius=1.0,color= red]";
+       }
+           
+      
+   }
+}
+    
+   console.log(circle. toString());     
+
+   console.log(circle. setcolor());
+
+   console.log(circle.radius,color);
